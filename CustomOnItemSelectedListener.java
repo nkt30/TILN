c0f9e322f7a0b212f80d44aee06d4fb3f47cf7dc
kn266 <kn266@msstate.edu>
2016-04-26 12:50:45 -0500

    Update CustomOnItemSelectedListener.java
    When you assign values to the numbers as strings the 0x will also come in multiple times.We don't want this. I took them out already.

--- CustomOnItemSelectedListener.java
+++ CustomOnItemSelectedListener.java
@@ -12,31 +12,31 @@ public class CustomOnItemSelectedListener implements OnItemSelectedListener {
     public String ColorHex;
 
     public void onItemSelected(AdapterView<?> parent, View view, int pos, String id) {
-        //add hex values for color here
+        //add hex values for color here <---- These values don't need the 0x in front of them you only need it in front of the final value
         switch (pos){
             case 0:
                 //white
-                ColorHex = "0x1";
+                ColorHex = "1";
                 break;
             case 1:
                 //red
-                ColorHex= "0x2";
+                ColorHex= "2";
                 break;
             case 2:
                 //green
-                ColorHex= "0x3";
+                ColorHex= "3";
                 break;
             case 3:
                 //blue
-                ColorHex= "0x4";
+                ColorHex= "4";
                 break;
             case 4:
                 //yellow
-                ColorHex= "0x5";
+                ColorHex= "5";
                 break;
             case 5:
                 //purple;
-                ColorHex= "0x6";
+                ColorHex= "6";
                 break;
         }
         //Toast.makeText(parent.getContext(), "Color : " + parent.getItemAtPosition(pos).toString(), Toast.LENGTH_SHORT).show();
